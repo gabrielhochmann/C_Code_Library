fdfed9f3e8f61b29e06e68a11e7bb0ed487c3a5d
Arkzesru <gabrielhochmannt@gmail.com>
2024-07-15 23:41:34 -0300

    Add recursive array reversal function

--- University/data_structure_and_algorithms/recursion.c
+++ University/data_structure_and_algorithms/recursion.c
@@ -131,10 +131,41 @@ char *powerSet(char *orig, int len, char *conj)
 {
     *orig = *conj;
 
-    return conj;
+    len = 1;
+
+    return conj + len;
+}
+
+int revertArrayRecursive(int v[], int start, int end)
+{
+    if (start >= end)
+    {
+        return 0;
+    }
+
+    int temp = v[end];
+    v[end] = v[start];
+    v[start] = temp;
+
+    return revertArrayRecursive(v, start + 1, end - 1);
+}
+
+void revertArray(int v[], int n)
+{
+    revertArrayRecursive(v, 0, n - 1);
 }
 
 int main()
 {
+    int v[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+    int n = sizeof(v) / sizeof(v[0]);
+
+    revertArray(v, n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", v[i]);
+    }
+
     return 0;
 }
